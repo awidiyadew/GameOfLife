8a10f56d9e12b39518ad2f2ea305cda1fb3e5f52
awidiyadew <awidiya.dewa@gmail.com>
2018-02-03 17:33:42 +0700

    checking equality of two Block

---
+++ src/Block.js
@@ -0,0 +1,22 @@
+export default class Block {
+  constructor(row, column) {
+    this._row = row;
+    this._column = column;
+  }
+
+  equals(otherBlock) {
+    if (!otherBlock) {
+      return false;
+    }
+
+    if (this._row !== otherBlock._row) {
+      return false;
+    }
+
+    if (this._column !== otherBlock._column) {
+      return false;
+    }
+
+    return true;
+}
+}
