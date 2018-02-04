5435046d52440d2844f75f5929f43d07ae0efa11
dewa.awidiya <dewa.awidiya@btpn.com>
2018-02-04 22:21:09 +0700

    should not addCell if input is undefined

--- test/Universe.test.js
+++ test/Universe.test.js
@@ -18,5 +18,24 @@ describe('Universe', () => {
       const actualAliveCells = universe.getAliveCell();
       expect(actualAliveCells.length).to.eq(1);
     });
+
+    it('add Cell{0, 0}, should return Cell{0, 0} with the data index', () => {
+      const universe = new Universe();
+      const block00 = new Block(0, 0);
+      const inputCell00 = new Cell(block00);
+      universe.addCell(inputCell00);
+
+      const aliveCellInUniverse = universe.getAliveCell();
+      const cellIndex = aliveCellInUniverse.length - 1;
+      const actualCellInUniverse = aliveCellInUniverse[cellIndex];
+      expect(actualCellInUniverse.equals(inputCell00)).to.be.true;
+    });
+
+    it('should not addCell if input is undefined', () => {
+      const universe = new Universe();
+      universe.addCell(undefined);
+      const cellInUniverse = universe.getAliveCell();
+      expect(cellInUniverse.length).to.eq(0);
+    });
   });
 });
