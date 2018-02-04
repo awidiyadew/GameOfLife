2bda93837c69968d2fc3140856c981e07d4b39cd
awidiyadew <awidiya.dewa@gmail.com>
2018-02-04 17:05:12 +0700

    check equality and ducktyping for cell

--- src/Cell.js
+++ src/Cell.js
@@ -2,11 +2,33 @@
  * Represent a living things in a universe
  */
 export default class Cell {
-  constructor(isAlive) {
+  constructor(block, isAlive) {
+    this._block = block;
     this._isAlive = isAlive;
   }
 
   isAlive() {
     return this._isAlive;
+  }
+
+  equals(otherCell) {
+    if (!otherCell) {
+      return false;
+    }
+
+    if (this._isDifferentType(otherCell)) {
+      return false;
+    }
+
+    if (!this._block.equals(otherCell._block)) {
+      return false;
+    }
+
+    return this._isAlive === otherCell._isAlive;
+  }
+
+  _isDifferentType(otherCell) {
+    const someBlock = otherCell._block;
+    return (!someBlock || otherCell._isAlive === undefined || otherCell._isAlive === null);
   }
 }
--- test/Cell.test.js
+++ test/Cell.test.js
@@ -1,16 +1,55 @@
 import Cell from '../src/Cell';
+import Block from '../src/Block';
 import { expect } from 'chai';
 
 describe('Cell', () => {
   describe('#isAlive', () => {
-    it('true if a cell instantiated with true', () => {
-      const cell = new Cell(true);
+    it('true if a cell instantiated with alive true', () => {
+      const cell = new Cell({}, true);
       expect(cell.isAlive()).to.be.true;
     });
 
-    it('false if a cell instantiated with false', () => {
-      const cell = new Cell(false);
+    it('false if a cell instantiated with alive false', () => {
+      const cell = new Cell({}, false);
       expect(cell.isAlive()).to.be.false;
+    });
+  });
+
+  describe('#equals', () => {
+    it('true if input are Cell({0, 0}, true) and Cell({0, 0}, true)', () => {
+      const blockCell = new Block(0, 0);
+      const cell = new Cell(blockCell, true);
+      const otherBlockCell = new Block(0, 0);
+      const otherCell = new Cell(otherBlockCell, true);
+      expect(cell.equals(otherCell)).to.be.true;
+    });
+
+    it('false if input are Cell({0, 0}, true) and Cell({1, 1}, true)', () => {
+      const blockCell = new Block(0, 0);
+      const cell = new Cell(blockCell, true);
+      const otherBlockCell = new Block(1, 1);
+      const otherCell = new Cell(otherBlockCell, true);
+      expect(cell.equals(otherCell)).to.be.false;
+    });
+
+    it('false if input are Cell({0, 0}, false) and Cell({0, 0}, true)', () => {
+      const blockCell = new Block(0, 0);
+      const cell = new Cell(blockCell, false);
+      const otherBlockCell = new Block(0, 0);
+      const otherCell = new Cell(otherBlockCell, true);
+      expect(cell.equals(otherCell)).to.be.false;
+    });
+
+    it('false if input are Cell({0, 0}, false) and undefined', () => {
+      const blockCell = new Block(0, 0);
+      const cell = new Cell(blockCell, false);
+      expect(cell.equals(undefined)).to.be.false;
+    });
+
+    it('false if input are Cell({0, 0}, false) and string something', () => {
+      const blockCell = new Block(0, 0);
+      const cell = new Cell(blockCell, false);
+      expect(cell.equals('something')).to.be.false;
     });
   });
 });
