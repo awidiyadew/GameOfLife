327ad03a38e020457383dd59ed4a4a946f0c7ceb
awidiyadew <awidiya.dewa@gmail.com>
2018-02-04 20:49:45 +0700

    findMyNeighboar and combine with input cell

--- src/Cell.js
+++ src/Cell.js
@@ -2,7 +2,7 @@
  * Represent a living things in a universe
  */
 export default class Cell {
-  constructor(block, isAlive) {
+  constructor(block, isAlive = true) {
     this._block = block;
     this._isAlive = isAlive;
   }
@@ -30,5 +30,42 @@ export default class Cell {
   _isDifferentType(otherCell) {
     const someBlock = otherCell._block;
     return (!someBlock || otherCell._isAlive === undefined || otherCell._isAlive === null);
+  }
+
+  _findNeighborBlock() {
+    return [
+      this._block.topLeftBlock(),
+      this._block.topBlock(),
+      this._block.topRightBlock(),
+      this._block.rightBlock(),
+      this._block.bottomRightBlock(),
+      this._block.bottomBlock(),
+      this._block.bottomLeftBlock(),
+      this._block.leftBlock()
+    ];
+  }
+
+  findMyNeighbors(someCells) {
+    const someBlocks = someCells.map((cell) => cell._block);
+    const neighborBlocks = this._findNeighborBlock();
+
+    let myNeighbors = [];
+    neighborBlocks.forEach((neighborBlock) => {
+      let isNeighborDead = true;
+      someBlocks.forEach((liveBlock, index) => {
+        const isNeighbor = liveBlock.equals(neighborBlock);
+        if (isNeighbor) {
+          isNeighborDead = false;
+          myNeighbors.push(someCells[index]);
+        }
+      });
+
+      if (isNeighborDead) {
+        const deadNeighbor = new Cell(neighborBlock._row, neighborBlock._column, false);
+        myNeighbors.push(deadNeighbor);
+      }
+    });
+
+    return myNeighbors;
   }
 }
--- test/Cell.test.js
+++ test/Cell.test.js
@@ -52,4 +52,22 @@ describe('Cell', () => {
       expect(cell.equals('something')).to.be.false;
     });
   });
+
+  describe('#findMyNeighbors', () => {
+    it('should not contain Cell{3, 3} if input is Cell{1, 1}', () => {
+      const cell11 = new Cell(new Block(1, 1));
+      const aCellInBoard = new Cell(new Block(0, 0));
+      const actualNeighbors = cell11.findMyNeighbors([aCellInBoard]);
+      const notANeighbor = new Cell(new Block(3, 3));
+      expect(actualNeighbors).to.deep.not.contains.members([notANeighbor]);
+    });
+
+    it('should contain Cell{0, 0} if target cell is Cell{1, 1} and input with some random Cell', () => {
+      const targetCell = new Cell(new Block(1, 1));
+      const aNeighbor = new Cell(new Block(0, 0));
+      const aCellInBoard = new Cell(new Block(0, 0));
+      const actualCellNeighbors = targetCell.findMyNeighbors([aCellInBoard]);
+      expect(actualCellNeighbors).to.deep.contains.members([aNeighbor]);
+    });
+  });
 });
